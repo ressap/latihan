9fb302886fa0294893931cefa7181f69829d033a
ressa <rpatrissia@gmail.com>
2018-09-04 19:14:05 +0700

    Initializing

---
+++ hal14.js
@@ -0,0 +1,16 @@
+// const angka = 2;
+// if(angka % 2 !== 0){
+// alert("angka" + angka +"ganjil");
+// } else {
+// alert("angka" + angka +"genap");
+// }
+
+
+const angka = 2;
+switch (true) {
+case (angka%2 == 0):
+console.log("genap"); break;
+default:
+console.log("ganjil");
+break;
+}
